6f4412542ed9b7bf435c52c9370733fb2d455adf
Sabet Maulana Mubarok <86289370+saabet@users.noreply.github.com>
2025-07-23 15:26:46 +0700

    style(pdf): refine PDF margins and formatting for better readability

--- controllers/pdfController.js
+++ controllers/pdfController.js
@@ -31,7 +31,7 @@ const generatePDF = async (request, h) => {
   if (!fs.existsSync(exportPath)) fs.mkdirSync(exportPath);
 
   const filePath = path.join(exportPath, `bill-${billId}.pdf`);
-  const doc = new PDFDocument();
+  const doc = new PDFDocument({ size: [299, 935.43], margins: { top: 40, bottom: 40, left: 14.17, right: 14.17 }});
   const stream = fs.createWriteStream(filePath);
   doc.pipe(stream);
 
@@ -40,25 +40,25 @@ const generatePDF = async (request, h) => {
     doc
       .font('Courier')
       .fontSize(10)
-      .text(`Struk belanja - ${billId}\n\n`, { width: 250, align: 'center' });
-    doc.text(`Nama: ${owner}\n\n${`-`.repeat(40)}`).moveDown(0.5);
+      .text(`Struk belanja - ${billId}\n\n`, { width: 270, align: 'center' });
+    doc.text(`Nama: ${owner}\n\n${`-`.repeat(45)}`).moveDown(0.5);
 
     data.items.forEach((item) => {
       const name = item.name.padEnd(17);
-      const quantity = String(item.quantity).padStart(5);
+      const quantity = String(item.quantity).padStart(7);
       const unitPrice = item.price.toLocaleString('id-ID').padStart(9);
-      const totalPrice = (item.quantity * item.price).toLocaleString('id-ID').padStart(9);
+      const totalPrice = (item.quantity * item.price).toLocaleString('id-ID').padStart(12);
       doc.text(`${name}${quantity}${unitPrice}${totalPrice}`).moveDown(0.5);
 
       if (item.discount > 0) {
         const discount = `-${item.discount.toLocaleString('id-ID')}`;
-        doc.text(`Hemat`.padEnd(40 - discount.length) + discount).moveDown(0.5);
+        doc.text(`Hemat`.padEnd(45 - discount.length) + discount).moveDown(0.5);
       }
     });
 
     doc.text(
-      `${`-`.repeat(40)}\n${`Total`.padEnd(
-        40 - data.total.toLocaleString('id-ID').length
+      `${`-`.repeat(45)}\n${`Total`.padEnd(
+        45 - data.total.toLocaleString('id-ID').length
       )}${data.total.toLocaleString('id-ID')}`
     );
 
